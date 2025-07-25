7b2d57a650d3bf2cf5ba463ac207735c07e1b977
Christopher Harshfield <47462417+charsh01@users.noreply.github.com>
2025-07-24 21:34:04 -0400

    Update similarity_betweenness_scatter-graph.py

--- similarity_betweenness_scatter-graph.py
+++ similarity_betweenness_scatter-graph.py
@@ -4,8 +4,10 @@ from matplotlib.patches import Patch
 
 df = pd.read_csv(r"C:\Users\cphar\.Neo4jDesktop\relate-data\dbmss\dbms-92f6f9c7-27e7-4e1f-b517-5760b2f2c472\import\between_sim_export.csv")
 
+# Scale the candidate dots
 sizes = df['contribution_totals'] / 1000000
 
+# Format the dots, most noteably with size and candidate color
 scatter = plt.scatter(
     x=df['candidate_betweenness'],
     y=df['similarity_avg'],
@@ -17,6 +19,7 @@ scatter = plt.scatter(
     linewidth=0.5
 )
 
+# Enumerate party for color mapping
 party_mapping = {
     1: "Republican",
     2: "Democrat",
@@ -26,6 +29,7 @@ party_mapping = {
     6: "Libertarian"
 }
 
+# Create labels
 plt.xlabel('candidate_betweenness')
 plt.ylabel('similarity_avg')
 plt.title('Scatter plot sized by contribution_totals, colored by Party')
@@ -33,7 +37,7 @@ plt.title('Scatter plot sized by contribution_totals, colored by Party')
 legend_elements = [
     Patch(facecolor=plt.cm.tab10(i-1), edgecolor='w', label=name)
     for i, name in party_mapping.items()
-    if i in df['Party'].values  # Only include if present in data
+    if i in df['Party'].values
 ]
 plt.legend(handles=legend_elements, title="Party")
 
